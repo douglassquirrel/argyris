7052da8375b001c2f72a6107a9c997ad08c3461a
Douglas Squirrel <ds@douglassquirrel.com>
2015-06-26 16:30:39 +0100

    record attributes

--- src/lib/argyris.js
+++ src/lib/argyris.js
@@ -1,16 +1,21 @@
 #!/usr/bin/env node
 SemanticUnit = function(text) {
     this.text = text;
-}
+    this.attributes = [];
+};
 
 SemanticUnit.prototype.toString = function() {
     return this.text;
-}
+};
 
 SemanticUnit.prototype.getConstituents = function() {
     return [];
-}
+};
 
 SemanticUnit.prototype.getAttributes = function() {
-    return [];
-}
+    return this.attributes;
+};
+
+SemanticUnit.prototype.recordAttribute = function(attribute) {
+    this.attributes.push(attribute);
+};
--- src/test/semantic-unit-test.js
+++ src/test/semantic-unit-test.js
@@ -1,27 +1,44 @@
 #!/usr/bin/env node
 require("../lib/argyris");
 
+function newWord(text) {
+    if (typeof(text)==='undefined') text = "testword";
+    return new SemanticUnit(text);
+};
+
 exports.testShouldRepresentAWord = function(test) {
     test.expect(1);
-    var blue = new SemanticUnit("blue");
+    var blue = newWord("blue");
     test.strictEqual(blue.toString(), "blue", "Word not represented");
     test.done();
 };
 
 exports.testAWordShouldHaveNoConstituents = function(test) {
     test.expect(1);
-    var grass = new SemanticUnit("grass");
-    var constituents = grass.getConstituents();
+    var word = newWord();
+    var constituents = word.getConstituents();
     test.strictEqual(constituents.length, 0,
                      "Word should not have constituents");
     test.done();
 };
 
-exports.testAUnitHasNoAttributesToStart = function(test) {
+exports.testAUnitShouldHaveNoAttributesToStart = function(test) {
     test.expect(1)
-    var river = new SemanticUnit("river");
-    var attributes = river.getAttributes();
+    var word = newWord();
+    var attributes = word.getAttributes();
     test.strictEqual(attributes.length, 0,
                      "New unit should have no attributes");
+    test.done();
+};
+
+exports.testAUnitShouldAcceptNewAttributes = function(test) {
+    test.expect(1)
+    var river = newWord("river");
+    river.recordAttribute("wet");
+    river.recordAttribute("meandering");
+    var attributes = river.getAttributes();
+    attributes.sort();
+    test.deepEqual(attributes, ["meandering", "wet"],
+                   "Unit should record attributes");
     test.done();
 };
