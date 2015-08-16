01a0ac45e97e5751eb5f4927d81180baaa0ee994
Douglas Squirrel <ds@douglassquirrel.com>
2015-08-16 19:45:27 +0100

    source data unconstrained

--- src/lib/construct_tree.js
+++ src/lib/construct_tree.js
@@ -4,7 +4,7 @@ var TreeModel = require("tree-model");
 
 var construct_tree = function(source_data, transformers) {
     tree = new TreeModel();
-    return tree.parse(source_data);
+    return tree.parse({data: source_data});
 }
 
 module.exports = construct_tree;
--- src/test/construct_tree_test.js
+++ src/test/construct_tree_test.js
@@ -9,8 +9,8 @@ var makeTree = function(model) {
 
 exports.shouldConstructTrivialTree = function(test) {
     test.expect(1);
-    var trivialTree = construct_tree({label: "root"}, []);
-    var expectedTree = makeTree({label: "root"});
+    var trivialTree = construct_tree(1, []);
+    var expectedTree = makeTree({data: 1});
     console.log(expectedTree.model);
     test.deepEqual(trivialTree.model, expectedTree.model);
     test.done();
